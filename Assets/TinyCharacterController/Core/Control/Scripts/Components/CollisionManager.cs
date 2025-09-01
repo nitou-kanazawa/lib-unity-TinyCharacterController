f3e6e395660bfa129dd79781e4fc9e368f78cbf7
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2025-09-01 23:17:25 +0900

    Manager関連のコンポーネントを追加

---
+++ Assets/TinyCharacterController/Core/Control/Scripts/Components/CollisionManager.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nitou.TCC.Controller.Core {
+
+    internal sealed class CollisionManager {
+
+        private readonly List<Collider> _ownColliders = new();        // Own collider (collider to be ignored when determining movement)
+
+
+        /// ----------------------------------------------------------------------------
+        // Public Method
+
+        /// <summary>
+        /// ����������
+        /// </summary>
+        public void Initialize(GameObject obj) {
+            obj.GetComponentsInChildren(_ownColliders);
+
+            IgnoreChildColliders(obj, true);
+        }
+
+        /// <summary>
+        /// To avoid contact between the Collider set on the parent object and the child Collider,
+        /// To prevent interference with the child Collider.
+        /// Without this process, the character could rise infinitely in contact with its own Collider.
+        /// </summary>
+        public void IgnoreChildColliders(GameObject obj, bool isIgnore) {
+            var childColliders = obj.GetComponentsInChildren<Collider>();
+            foreach (var ownCollider in _ownColliders) {
+                foreach (var col in childColliders) {
+                    if (_ownColliders.Contains(col))
+                        continue;
+
+                    Physics.IgnoreCollision(col, ownCollider, isIgnore);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/TinyCharacterController/Core/Control/Scripts/Components/UpdateComponentManager.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Nitou.TCC.Controller.Interfaces.Core;
+using Nitou.TCC.Utils;
+
+namespace Nitou.TCC.Controller.Core
+{
+    internal sealed class UpdateComponentManager 
+    {
+        // List of components to be updated at runtime
+        private readonly List<IUpdateComponent> _updates = new(); 
+
+        public bool IsInitialized { get; private set; } = false;
+
+
+        // ----------------------------------------------------------------------------
+        // Public Method
+
+        /// <summary>
+        /// 初期化処理．
+        /// </summary>
+        public void Initialize(GameObject obj)
+        {
+            obj.GetComponentsInChildren(_updates);
+            _updates.Sort((a, b) => a.Order - b.Order);
+
+            IsInitialized = true;
+        }
+
+        /// <summary>
+        /// 更新処理．
+        /// </summary>
+        public void Process(float deltaTime)
+        {
+            using var _ = new ProfilerScope("Component Update");
+            foreach (var update in _updates)
+            {
+                update.OnUpdate(deltaTime);
+            }
+        }
+    }
+}
